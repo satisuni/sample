d49d69873ce8cd29505180ec9b20a289de86f14e
satish <satish@IDEA-1049>
2015-03-19 10:12:54 +0530

    committed

---
+++ Sample/src/HelloWorld.java
@@ -0,0 +1,25 @@
+import java.text.SimpleDateFormat;
+
+import java.util.Date;
+
+public class HelloWorld {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+			System.out.print("hello world\n");
+			int a=10;
+			int b=10;
+			System.out.print(a+b + "\n");
+			
+			String ss="hello world";
+			int index=ss.indexOf("lo");
+			System.out.print(index+ "\n");
+			Date date = new Date();
+		      String strDateFormat = "HH:mm:ss a";
+		      SimpleDateFormat sdf = new SimpleDateFormat(strDateFormat);
+		      System.out.println(sdf.format(date));
+ 
+					
+	}
+
+}
